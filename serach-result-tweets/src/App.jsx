c70c4c87048576a9aa976c8a224cef771e51d662
santiagoalex <santiagos9511@gmail.com>
2023-02-05 16:38:03 -0500

    Delete redundant code

--- serach-result-tweets/src/App.jsx
+++ serach-result-tweets/src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import SearchResult from "./components/TwetSearchResults";
 import mock from "./assets/mock-data.json";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <SearchResult tweets={mock} />
